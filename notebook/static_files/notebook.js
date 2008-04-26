6debc12f4ee838c3f6d4421352793d3afac38a39
Juha Autero <jautero@iki.fi>
2008-04-26 13:12:46 +0300

    Completed change to templates. This also changed behaviour of notes, because template has different kind of behaviour. Next steps are fix the documentation, redesign templates and implement the save functionality.

--- notebook/static_files/notebook.js
+++ notebook/static_files/notebook.js
@@ -13,11 +13,11 @@ function add(title,text,editit) {
   node.id=id;
   for (var i = node.childNodes.length - 1; i >= 0; i--){
   	var element=node.childNodes[i];
-	if (element.getAttribute("class")=="title") {
+	if (element.getAttribute && element.getAttribute("class")=="title") {
 		element.setAttribute("id",id+"t");
 		element.innerHTML=title;
 	}
-	if (element.getAttribute("class")=="content") {
+	if (element.getAttribute && element.getAttribute("class")=="content") {
 		element.setAttribute("id",id+"c");
 		element.innerHTML=text;
 	}
@@ -40,18 +40,34 @@ function editAll() {
   return true;
 }
 function replace_node_with_edit_fragment(node,title,content) {
-	var editnode=document.getElementById('edittemplate').cloneNode(true);
 	editnode.id=node.id
 	var titlename= node.id +'t';
 	var contentname = node.id + 'c';
 }
 
+function get_node_by_class(node,aclass) {
+	for (var i=0; i < node.childNodes.length; i++) {
+		var child=node.childNodes[i];
+		if (child.getAttribute && child.getAttribute("class")==aclass) {
+			return child;
+		};
+	};
+	return null;
+}
 function edit(node) {
-  var divs=node.getElementsByTagName('div');
-  var quote="'";
-  var title=divs[0].innerHTML;
-  var content=divs[2].innerHTML;
-  
+  var titlediv=get_node_by_class(node,"title");
+  var contentdiv=get_node_by_class(node,"content");
+  var editnode=document.getElementById('edittemplate').cloneNode(true);
+  var titleelement=get_node_by_class(editnode,"title");
+	var contentelement=get_node_by_class(editnode,"content");
+	
+  editnode.id=node.id;
+	titleelement.name=node.id+"t";
+	contentelement.name=node.id+"c";
+	
+	titleelement.value=titlediv.innerHTML;
+	contentelement.value=contentdiv.innerHTML;
+	node.parentNode.replaceChild(editnode,node);
 }
 
 function getValueOfNamedChild(node,name)
@@ -67,9 +83,25 @@ function getValueOfNamedChild(node,name)
 function done(node) {
   var title=getValueOfNamedChild(node,node.id+"t");
   var content=getValueOfNamedChild(node,node.id+"c");
-  node.innerHTML='<div class="menu" onclick="edit(this.parentNode);">Edit</div><div class="title">'+title+
-                 '</div><div class="content">'+content+'</div>';
-
+  var noteelement=document.getElementById('notetemplate').cloneNode(true);
+	var newtitle=get_node_by_class(noteelement,"title");
+	var newcontent=get_node_by_class(noteelement,"content");
+	
+	noteelement.id=node.id;
+	newtitle.setAttribute("id",node.id);
+	newtitle.innerHTML=title;
+	newcontent.setAttribute("id",node.id);
+	newcontent.innerHTML=content;
+	
+	node.parentNode.replaceChild(noteelement,node);
 }
 
 
+function toggle_content(node) {
+	var contentnode=get_node_by_class(node,"content");
+	if (contentnode.style.display == 'none') {
+		contentnode.style.display = 'block';
+	} else {
+		contentnode.style.display = 'none';
+	}
+}
