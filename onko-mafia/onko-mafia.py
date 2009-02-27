c683ceaf0c7b12bfef0a40a85d39a6c433304586
Juha Autero <jautero@iki.fi>
2009-02-28 00:50:38 +0200

    Added convert_to_text and classes for creating feeds.

--- onko-mafia/onko-mafia.py
+++ onko-mafia/onko-mafia.py
@@ -68,26 +68,62 @@ def onko_mafia_day(date):
     else:
         return False
 
+def convert_to_text(truth_value):
+  if truth_value:
+    return "on"
+  else:
+    return "ei"
+
 class OnkoMafia(webapp.RequestHandler):
 
     def get(self):
         template_values=dict(globals())
         mydate=datetime.date.today()
-        if onko_mafia_week(mydate):
-            template_values["weekclass"]="on"
-            template_values["weekresult"]="on"
-        else:
-            template_values["weekclass"]="ei"
-            template_values["weekresult"]="ei"
-        if onko_mafia_day(mydate):
-            template_values["dayclass"]="on"
-            template_values["dayresult"]="on"
-        else:        
-            template_values["dayclass"]="ei"
-            template_values["dayresult"]="ei"
+        template_values["weekclass"]=convert_to_text(onko_mafia_week(mydate))
+        template_values["weekresult"]=convert_to_text(onko_mafia_week(mydate))
+        template_values["dayclass"]=convert_to_text(onko_mafia_day(mydate))
+        template_values["dayresult"]=convert_to_text(onko_mafia_day(mydate))
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
+class FeedItem:
+  def __init__(self,date,weekly):
+    self.date=date
+    self.day=onko_mafia_day(date)
+    self.week=onko_mafia_week(date)
+    self.weekly=weekly
+  
+  def init_feed_dict(self):
+    feed_dict={}
+    if self.weekly:
+      feed_dict["title"]="Onko mafia tällä viikolla?"
+      feed_dict["answer"]=convert_to_text(self.week)
+    else:
+      feed_dict["title"]="Onko mafia tänään?"
+      feed_dict["answer"]=convert_to_text(self.day)
+    feed_dict["date"]=self.date.ctime()
+    return feed_dict
+    
+  def get_rss(self):
+    return "<item><title>%(title)s</title><description>%(answer)s</description></item>" % self.init_feed_dict()
+    
+daydelta=datetime.timedelta(days=1)
+weekdelta=7*daydelta
+
+class Feed:
+  def __init__(self,count=10,date=None,weekly=False):
+    if not date:
+      date=datetime.date.today()
+    if weekly:
+      date=date-date.weekday()*daydelta
+    self.items=[]
+    for i in range(0,count):
+      self.items.append(FeedItem(date),weekly)
+      if weekly:
+        date=date-weekdelta
+      else:
+        date=date-daydelta
+      
 def main():
     application = webapp.WSGIApplication([('/', OnkoMafia)],
                                         debug=True)
