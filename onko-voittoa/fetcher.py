4ac8208b178c6c50c1caed0a19034bfe5c391601
Juha Autero <jautero@iki.fi>
2010-06-13 12:31:18 +0300

    Reformatted results fetching to use task queues to continue fetching until successful.
    Decreased font size and added colon to improve readability. Though it is still ugly.

--- onko-voittoa/fetcher.py
+++ onko-voittoa/fetcher.py
@@ -1,4 +1,5 @@
 from lottorivitmodel import VoittoRivi
+from google.appengine.api.labs import taskqueue
 from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from config import lottourl
@@ -7,6 +8,11 @@ import datetime
     
 class RivitHandler(webapp.RequestHandler):
     def get(self):
+        tuorein_kierros=VoittoRivi.gql("ORDER BY vuosi DESC,kierros DESC").get().kierros
+        taskqueue.add(self.request.path,params={"kierros":tuorein_kierros})
+            
+    def post(self):
+        edellinen_kierros=self.request.get("kierros",None)
         parser=LottoPageParser()
         response=urlfetch.fetch(lottourl)
         if response.status_code == 200:
@@ -15,5 +21,9 @@ class RivitHandler(webapp.RequestHandler):
             year=datetime.date.today().year
             if kierros>5 and datetime.date.today().month==1:
                 year-=1
-            rivi=VoittoRivi(kierros=kierros,vuosi=year,numerot=parser.numerot,lisanumerot=parser.lisanumerot)
-            rivi.put()
+            if edellinen_kierros and kierros != edellinen_kierros
+                rivi=VoittoRivi(kierros=kierros,vuosi=year,numerot=parser.numerot,lisanumerot=parser.lisanumerot)
+                rivi.put()
+            else:
+                self.error(500)
+                
