b299d0add114761e25a1a69d8ebdf8d3a12c8b72
Juha Autero <jautero@iki.fi>
2010-08-23 23:41:40 +0300

    Simple demo version of Hacklab soda tracking.

---
+++ limukassa/limukassa.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Copyright 2008 Juha Autero
+#
+# Copyright 2010 Juha Autero <jautero@iki.fi>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+project="Limukassa"
+version="1.0"
+author="Juha Autero <jautero@iki.fi>"
+copyright="Copyright 2010 Juha Autero <jautero@iki.fi>"
+application="limukassa"
+import wsgiref.handlers
+import os
+
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+
+class Limukassa(webapp.RequestHandler):
+
+  def get(self):
+    template_values=globals()
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
+  def post(self):
+    userid=self.request.get("userid",None)
+    productid=self.request.get("productid",None)
+    template_values=globals()
+    template_values["userid"]=userid
+    template_values["productid"]=productid
+    if userid and productid:
+      path = os.path.join(os.path.dirname(__file__), 'done.html')      
+    else:
+      path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
+    
+
+def main():
+  application = webapp.WSGIApplication([('/', Limukassa)],
+                                       debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+  main()
---
+++ limukassa/static_files/limukassa.js
@@ -0,0 +1,5 @@
+// Javascript for limukassa
+//
+// Copyright 2010 Juha Autero <jautero@iki.fi>
+//
+//
